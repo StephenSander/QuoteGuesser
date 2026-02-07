54c296fb085162a6e940e22ee31cf391fd51da2c
Stephen S <stephenmsander@yahoo.com>
2026-02-07 13:48:50 -0800

    Come back here if objects dont work

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ function generateRandomNumber(num) {
 };
 
 const quotes = [
-    ['Obi-Wan Kenobi', 'May the Force be with you.', 'Star Wars'],
-    ['The Terminator', "I'll be back", 'Terminator'],
-    ['The Joker', 'Why so serious?', 'The Dark Knight'],
-    ['Tony Montana', 'Say hello to my little friend!', 'Scarface'],
-    ['Announcer', 'Finish him!', 'Mortal Kombat'],
-    ['Dorothy', "There's no place like home", 'The Wizard of Oz'],
-    ['GLaDOS', 'The cake is a lie', 'Portal'],
-    ['Mario', "It's-a-me", 'Super Mario Bros'],
-    ['Gandalf', 'You shall not pass!', 'The Lord of the Rings'],
-    ['Darth Vader', 'I am your father', 'Star Wars'],
-    ['Andy Dufresne', 'Get busy living, or get busy dying', 'The Shawshank Redemption'],
-    ['Uncle Ben', 'With great power comes great responsibility', 'Spider-Man'],
-    ['Doc Brown', "Roads? Where we're going, we don't need roads", "Back to the Future"],
-    ['Vladimir Makarov', 'Remember...No Russion', 'Call of Duty: Modern Warfare 2'],
-    ['William Wallace', "They may take our lives, but they'll never take our freedom!", 'Braveheart'],
-    ['Harvey Dent', 'You either die a hero or live long enough to see yourself become the villain', 'The Dark Knight'],
-    ['King Leonidas', 'This is Sparta!', '300'],
-    ['Master Chief', 'I need a weapon', 'Halo 2'],
-    ['Captain Jack Sparrow', "I've got a jar of dirt", 'Pirates of the Carribean']
+    ['Obi-Wan Kenobi', 'May the Force be with you.', 'Star Wars', 'movie'],
+    ['The Terminator', "I'll be back", 'Terminator', 'movie'],
+    ['The Joker', 'Why so serious?', 'The Dark Knight', 'movie'],
+    ['Tony Montana', 'Say hello to my little friend!', 'Scarface', 'movie'],
+    ['Announcer', 'Finish him!', 'Mortal Kombat', 'game'],
+    ['Dorothy', "There's no place like home", 'The Wizard of Oz', 'movie'],
+    ['GLaDOS', 'The cake is a lie', 'Portal', 'game'],
+    ['Mario', "It's-a-me", 'Super Mario Bros', 'game'],
+    ['Gandalf', 'You shall not pass!', 'The Lord of the Rings', 'movie'],
+    ['Darth Vader', 'I am your father', 'Star Wars', 'movie'],
+    ['Andy Dufresne', 'Get busy living, or get busy dying', 'The Shawshank Redemption', 'movie'],
+    ['Uncle Ben', 'With great power comes great responsibility', 'Spider-Man', 'movie'],
+    ['Doc Brown', "Roads? Where we're going, we don't need roads", "Back to the Future", 'movie'],
+    ['Vladimir Makarov', 'Remember...No Russion', 'Call of Duty: Modern Warfare 2', 'game'],
+    ['William Wallace', "They may take our lives, but they'll never take our freedom!", 'Braveheart', 'movie'],
+    ['Harvey Dent', 'You either die a hero or live long enough to see yourself become the villain', 'The Dark Knight', 'movie'],
+    ['King Leonidas', 'This is Sparta!', '300', 'movie'],
+    ['Master Chief', 'I need a weapon', 'Halo 2', 'game'],
+    ['Captain Jack Sparrow', "I've got a jar of dirt", 'Pirates of the Carribean', 'movie']
 ]
 
 let gameQuote = [];
@@ -35,7 +35,16 @@ const getQuote = (array) => {
     gameQuote.push(array[generateRandomNumber(array.length)][1]);
     // Grab a random source
     gameQuote.push(array[generateRandomNumber(array.length)][2]);
-    console.log(gameQuote);
+
+    let author = gameQuote[0];
+    let saying = gameQuote[1];
+    let source = gameQuote[2];
+    const findSourceType = () => {
+        
+    }
+
+    console.log('Okay are you ready? Here is your quote:');
+    console.log(`"${saying}" - ${author} from the ${sourceType}: ${source}`);
 };
 
 getQuote(quotes);
