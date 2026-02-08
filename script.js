97bdfa9dc6c660653c2f4b4d6ca048026cca653a
Stephen S <stephenmsander@yahoo.com>
2026-02-07 17:12:35 -0800

    added a webpage script and more quotes

--- script.js
+++ script.js
@@ -17,12 +17,15 @@ const quotes = [
     ['Andy Dufresne', 'Get busy living, or get busy dying', {movie: 'The Shawshank Redemption'}],
     ['Uncle Ben', 'With great power comes great responsibility', {movie: 'Spider-Man'}],
     ['Doc Brown', "Roads? Where we're going, we don't need roads", {movie: "Back to the Future"}],
-    ['Vladimir Makarov', 'Remember...No Russion', {game: 'Call of Duty: Modern Warfare 2'}],
+    ['Vladimir Makarov', 'Remember...No Russian', {game: 'Call of Duty: Modern Warfare 2'}],
     ['William Wallace', "They may take our lives, but they'll never take our freedom!", {movie: 'Braveheart'}],
     ['Harvey Dent', 'You either die a hero or live long enough to see yourself become the villain', {movie: 'The Dark Knight'}],
     ['King Leonidas', 'This is Sparta!', {movie: '300'}],
     ['Master Chief', 'I need a weapon', {game: 'Halo 2'}],
-    ['Captain Jack Sparrow', "I've got a jar of dirt", {movie: 'Pirates of the Carribean'}]
+    ['Captain Jack Sparrow', "I've got a jar of dirt", {movie: 'Pirates of the Carribean'}],
+    ['Vaas Montenegro', 'Have I ever told you the definition of insanity?', {game: 'Far Cry 3'}],
+    ['Arthur Morgan', "It weren't us who changed", {game: 'Red Dead Redemption 2'}],
+    ['The Arbiter', 'Kill me or release me, parasite! But do not waste my time with talk', {game: 'Halo 2'}]
 ]
 
 let gameQuote = [];
@@ -57,7 +60,8 @@ const getQuote = (array) => {
     let sourceType = findSourceType();
 
     console.log('Okay are you ready? Here is your quote:');
-    console.log(`"${saying}" - ${author} from the ${sourceType}: ${source}`);
+    console.log(`"${saying}" - ${author}`)
+    console.log(`From the ${sourceType}: ${source}`);
 };
 
 getQuote(quotes);
